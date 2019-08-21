fad09b1779b225b9bbea3ba378c8edd67a86fd03
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-08-20 23:05:13 -0400

    going bananas

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -85,7 +85,14 @@ console.log(multiplyNums(5, 10, multiply));
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+ return cb(item, list);
 }
+
+const checkItems = (item, list) => {
+  return list.includes(item);
+};
+  
+console.log(contains )
 
 /* STRETCH PROBLEM */
 
